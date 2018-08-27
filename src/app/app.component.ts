63808f5f470fe215abbf1d9bc0e7216217339aec
colin bates <dragonflylord@gmail.com>
2018-08-27 11:40:38 -0700

    Add user name and password field to log in on home page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Player } from './player.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent 
+{
+  masterPlayer: Player = null;
 }
