ee6913540067663101b7e50bf652aef1bc825e8b
Alex Bunnell, Brain Palowski, Colin Bates, Noah Medoff, Nick Rogers and Randy Weyer <alex@>
2018-08-28 14:47:44 -0700

    Update names.

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -18,7 +18,19 @@ export class BattleComponent implements OnInit {
 
   activePlayer: Player[];
   constructor(private router: Router, private playerService: PlayerService) { }
-  currentActivePlayer;
+  currentActivePlayer: Player = <Player>
+  {
+    username: "",
+    userpassword: "",
+    level: 0,
+    idleAttack: 0,
+    attack: 0,
+    critChance: 0,
+    criticalDamage: 0,
+    gold: 0,
+    goldRate: 0,
+    isActive: false
+  }
 
   ngOnInit() {
     let playerList = this.playerService.getPlayers();
@@ -33,20 +45,50 @@ export class BattleComponent implements OnInit {
     })
   }
 
-  CurrentEnemyName() {
-    let enemyNameArray = ["Frodo", "Heidegger", "Snider", "Thundacheese", "Deviant", "Typical", "Normie"];
-    let enemyNameIndex = Math.floor(Math.random() * 7);
-    return enemyNameArray[enemyNameIndex];
+  currentEnemyName() {
+    let enemyNameArray = ["Frodo", "Heidegger", "Snider", "Thundacheese", "Deviant", "Typical", "Normie", "Thrash-Ram", "Hambone", "Goat-Faced Killah", "Magma", "Miscreant", "Peculiar", "Passive", "Impure", "Parvo-Dog", "Global Planetary Analysis Golem", "Preliminary Piloting Android", "Unified Shepherd Cyborg", "General Travel Golem", "Communist", "Electronic Lifeform Detection Technician", "Extraterrestial Personal Protection Bot", "Sensitive Operating Emulator", "Neohuman Emergency Response Juggernaut", "Digital Processor Juggernaut"];
+    let bossName = ["Super", "Mega", "Ultra", "Milky", "Amorphous", "Sexy", "Uncomfortable","Uber", "Hyper", "Master", "Godly", "Massive", "Deadly", "Mechanized", "Strange", "Sweaty", "Pluralized", "Huge"];
+    let enemyNameIndex = Math.floor(Math.random() * 25);
+    let bossIndex = Math.floor(Math.random() * 15);
+    if(this.enemyIsBoss()){
+      return bossName[bossIndex] + " " + enemyNameArray[enemyNameIndex];
+    } else {
+      return enemyNameArray[enemyNameIndex];
+    }
   }
   enemyIsBoss() {
-    if(currentActivePlayer.level % 10 === 0){
+    if(this.currentActivePlayer.level % 10 === 0){
       return true;
     } else {
       return false;
     }
   }
-
-  let currentLevelEnemy = new Enemy()
+  enemyDefense() {
+    return Math.floor(this.currentActivePlayer.level/5);
+  }
+  enemyHitPoints() {
+    let hitPoints = 50 + (Math.pow(10, (Math.floor(this.currentActivePlayer.level/10))));
+    if(this.enemyIsBoss()){
+      hitPoints*=10;
+    }
+    return hitPoints;
+  }
+  enemyHealthRegen(){
+    let healthRegen = Math.floor(this.enemyHitPoints() / 100);
+    if(this.enemyIsBoss()){
+      return healthRegen;
+    } else {
+      return 0;
+    }
+  }
+  currentLevelEnemy: Enemy = <Enemy>
+  {
+    name: this.currentEnemyName(),
+    isBoss: this.enemyIsBoss(),
+    defense: this.enemyDefense(),
+    hitPoints: this.enemyHitPoints(),
+    healthRegen: this.enemyHealthRegen()
+  }
 
   // let testPlayer = new Player("randy", 3, 3, 3, 1, 3, 120, 1);
   // let testEnemy = new Enemy("colin", false, 1, 100, 1);
